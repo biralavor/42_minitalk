345a18b6dc51cabcfaad8073af0fa4048580e950
umeneses <umeneses@student.42sp.org.br>
2024-04-19 19:28:59 -0300

    first use of SUGUSR1, not working

--- src/main.c
+++ src/main.c
@@ -5,37 +5,36 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/04/13 15:06:47 by umeneses          #+#    #+#             */
-/*   Updated: 2024/04/15 13:26:53 by umeneses         ###   ########.fr       */
+/*   Created: 2024/04/18 13:06:25 by umeneses          #+#    #+#             */
+/*   Updated: 2024/04/19 16:47:27 by umeneses         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "minitalk.h"
+#include <stdio.h>
+#include <unistd.h>
+#include <signal.h>
+#include <stdlib.h>
 
-void	handler(int sig)
+void	handler_sigusr1(int sig)
 {
-	write(STDOUT_FILENO, "NOT today!!! HAHAHAHA\n", 22);
+	(void)sig;
+	printf("User 1\n");
 }
 
 int	main(int argc, char **argv)
 {
-	char	*client_char;
+	struct sigaction	sigact;
 
-	client_char = NULL;
 	if (argc == 2)
 	{
-		client_char = argv[1];
-		signal(SIGINT, handler);
 		while (1)
 		{
-			ft_printf("client char = %c -> pid[%d}\n", client_char, getpid());
+			sigemptyset(&sigact.sa_mask);
+			sigact.sa_handler = &handler_sigusr1;
+			printf("Starting...\n");
+			sigaction(SIGUSR1, &sigact, NULL);
 			sleep(1);
 		}
-		return (0);
 	}
-	else
-	{
-		ft_printf("Usage: ./a.out [char]\n");
-		return (1);
-	}
+	return (0);
 }
