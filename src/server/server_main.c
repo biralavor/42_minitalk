1604b188610ac424955c449cf9d6c34d06309c32
umeneses <umeneses@student.42sp.org.br>
2024-04-20 17:45:15 -0300

    updating server files --wip

--- src/server/server_main.c
+++ src/server/server_main.c
@@ -6,33 +6,49 @@
 /*   By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/18 13:06:25 by umeneses          #+#    #+#             */
-/*   Updated: 2024/04/20 12:31:02 by umeneses         ###   ########.fr       */
+/*   Updated: 2024/04/20 17:38:51 by umeneses         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-#include <unistd.h>
-#include <signal.h>
-#include <stdlib.h>
+#include "minitalk.h"
 
-void	handler_sigusr1(int sig)
+static void	ft_msg_error(char *str);
+
+static void	handler_act(int sig, siginfo_t *info, void *ucontext)
 {
-	(void)sig;
-	printf("User 1\n");
+	(void)ucontext;
+	(void)info;
+	if (sig == SIGUSR1)
+	{
+		ft_putstr_fd("User 1\n", STDOUT_FILENO);
+	}
+	
 }
 
 int	main(void)
 {
-	struct sigaction	sigact;
+	struct sigaction	server_act;
 
+	sigemptyset(&server_act.sa_mask);
+	server_act.sa_flags = SA_SIGINFO;
+	server_act.sa_sigaction = handler_act;
+	if ((sigaction(SIGUSR1, &server_act, NULL) == SERVER_FAIL)
+		|| (sigaction(SIGUSR2, &server_act, NULL) == SERVER_FAIL))
+		ft_msg_error("Error >> Server signal failed.\n");
+	ft_putstr_fd("Server is Starting...\n", STDOUT_FILENO);
+	ft_putstr_fd("Server PID: ", STDOUT_FILENO);
+	ft_putnbr_fd(getpid(), STDOUT_FILENO);
+	ft_putstr_fd("\n\n", STDOUT_FILENO);
+	while (WAIT)
+	{
+		ft_putstr_fd("Server is Running...\n", STDOUT_FILENO);
+		sleep(2);
+	}
+	exit(EXIT_SUCCESS);
+}
 
-		while (1)
-		{
-			sigemptyset(&sigact.sa_mask);
-			sigact.sa_handler = &handler_sigusr1;
-			printf("Server is Starting...\n");
-			sigaction(SIGUSR1, &sigact, NULL);
-			sleep(1);
-		}
-	return (0);
+static void	ft_msg_error(char *str)
+{
+	ft_putstr_fd(str, STDERR_FILENO);
+	exit(EXIT_FAILURE);
 }
