837660572d11ea200350197f6f5e63524c13f9d8
umeneses <umeneses@student.42sp.org.br>
2024-04-20 17:44:20 -0300

    updating header - wip

--- headers/minitalk.h
+++ headers/minitalk.h
@@ -6,17 +6,49 @@
 /*   By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/15 12:49:06 by umeneses          #+#    #+#             */
-/*   Updated: 2024/04/15 14:41:10 by umeneses         ###   ########.fr       */
+/*   Updated: 2024/04/20 17:41:11 by umeneses         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINITALK_H
 # define MINITALK_H
 
-# include "../libs/libft/libft.h"
+# include "../libs/libft/includes/libft.h"
 # include <stdio.h>
 # include <unistd.h>
 # include <signal.h>
 # include <fcntl.h>
+
+enum e_server
+{
+	BYTE_SIZE = 8,
+	BIT = 1,
+	SERVER_FAIL = -1,
+	RESET = 0,
+	WAIT = 1,
+};
+
+enum e_client
+{
+	CLIENT_FAIL = -1,
+};
+
+enum e_validation
+{
+	ARG_SIZE = 2,
+	PID = 1,
+};
+
+enum e_signal
+{
+	PENDING_NO = 0,
+	PENDING_YES = 1,	
+};
+
+int	arg_validation(int argc, char *pid_input);
+/* non-static client functions */
+
+
+/* non-static server functions */
 
 #endif
