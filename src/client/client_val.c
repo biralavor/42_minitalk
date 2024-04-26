59431ccf236c35fafcfd55d6589b03ee3820d530
umeneses <umeneses@student.42sp.org.br>
2024-04-26 19:18:17 -0300

    updating feedback message

--- src/client/client_val.c
+++ src/client/client_val.c
@@ -6,7 +6,7 @@
 /*   By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/20 16:44:06 by umeneses          #+#    #+#             */
-/*   Updated: 2024/04/23 14:43:04 by umeneses         ###   ########.fr       */
+/*   Updated: 2024/04/26 19:17:29 by umeneses         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,8 @@ pid_t	arg_validation(int argc, char *pid_input)
 	{
 		ft_putstr_fd(RED, STDOUT_FILENO);
 		ft_putstr_fd("Please, hit on client's terminal:\n", STDOUT_FILENO);
-		ft_putstr_fd("\n./client server_PID\n", STDOUT_FILENO);
+		ft_putstr_fd(YELLOW, STDOUT_FILENO);
+		ft_putstr_fd("\n./client server_PID YOUR_MESSAGE\n", STDOUT_FILENO);
 		ft_error_msg("Invalid number of arguments.\n");
 		exit(EXIT_FAILURE);
 	}
@@ -30,8 +31,8 @@ pid_t	arg_validation(int argc, char *pid_input)
 		if (!ft_isdigit(*pid_input++))
 		{
 			ft_putstr_fd(RED, STDOUT_FILENO);
-			ft_putstr_fd("PID should be a number. ", STDOUT_FILENO);
-			ft_error_msg("Check your Server's terminal.\n");
+			ft_putstr_fd("PID has be a number. ", STDOUT_FILENO);
+			ft_error_msg("Check it on Server's terminal.\n");
 		}
 	}
 	return (server_pid);
