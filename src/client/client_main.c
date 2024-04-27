857b8512729b976ed9183f9c93294662666d204a
umeneses <umeneses@student.42sp.org.br>
2024-04-27 13:36:29 -0300

    NOW WORKING with any char. Still need some attetion for sending strings

--- src/client/client_main.c
+++ src/client/client_main.c
@@ -6,7 +6,7 @@
 /*   By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/18 13:06:25 by umeneses          #+#    #+#             */
-/*   Updated: 2024/04/26 21:24:32 by umeneses         ###   ########.fr       */
+/*   Updated: 2024/04/27 13:19:26 by umeneses         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,22 +32,26 @@ static int	ft_connection(int income, int check)
 
 void	cl_send_bit(int server_pid, char c_msg)
 {
-	int	bit_index;
-	int	bit_signal;
+	int		bit_index;
+	char	bit_signal;
 
-	bit_index = BYTE_SIZE;
-	while (--bit_index >= SIG_DEFAULT)
+	bit_index = 0;
+	while (bit_index < BYTE_SIZE)
 	{
 		ft_connection(SIG_DEFAULT, SIG_SET);
 		bit_signal = (c_msg >> bit_index & BIT);
+		ft_putnbr_fd(bit_signal, STDOUT_FILENO);
 		if (bit_signal == SIG_PENDING_NO)
 		{
 			if (kill(server_pid, SIGUSR1) == CLIENT_FAIL)
 				ft_error_msg("Server is offline. Run './server' again\n");
 		}	
 		else if (bit_signal == SIG_PENDING_YES)
+		{
 			if (kill(server_pid, SIGUSR2) == CLIENT_FAIL)
 				ft_error_msg("Server is offline. Run './server' again\n");
+		}
+		bit_index++;
 		while (!ft_connection(SIG_DEFAULT, SIG_GET))
 			;
 	}
@@ -60,14 +64,9 @@ void	cl_init(int server_pid)
 	ft_putstr_fd("Now, trying to connect with Server's PID: ", STDOUT_FILENO);
 	ft_putstr_fd(CYAN, STDOUT_FILENO);
 	ft_putnbr_fd(server_pid, STDOUT_FILENO);
-	// if (kill(server_pid, SIGUSR1) == CLIENT_FAIL
-	// 	|| kill(server_pid, SIGUSR2) == CLIENT_FAIL)
-	// 	ft_error_msg("Client failed to send signal to Server.\n");
 	ft_putstr_fd("\n", STDOUT_FILENO);
 	ft_putstr_fd(YELLOW, STDOUT_FILENO);
 	ft_putstr_fd("Client is running...\n", STDOUT_FILENO);
-	ft_putstr_fd("Type a new message:\n", STDOUT_FILENO);
-	ft_putstr_fd(">> ", STDOUT_FILENO);
 }
 
 int	main(int argc, char **argv)
@@ -79,8 +78,6 @@ int	main(int argc, char **argv)
 	next_char = 0;
 	server_pid = arg_validation(argc, argv[ARG_PID]);
 	cl_init(server_pid);
-	// sigemptyset(&client_act.sa_mask);
-	// client_act.sa_flags = SA_SIGINFO;
 	ft_memset(&client_act, SIG_DEFAULT, sizeof(client_act));
 	client_act.sa_handler = server_signal_handler;
 	client_act.sa_flags = SIG_DEFAULT;
@@ -88,9 +85,13 @@ int	main(int argc, char **argv)
 		ft_error_msg("Client signal failed.\n");
 	while (argv[ARG_MSG][next_char] != '\0')
 	{
+		ft_putstr_fd("\nSending the char >> ", STDOUT_FILENO);
+		ft_putchar_fd(argv[ARG_MSG][next_char], STDOUT_FILENO);
+		// ft_putchar_fd('\n', STDOUT_FILENO);
+		ft_putstr_fd(" >> ", STDOUT_FILENO);
 		cl_send_bit(server_pid, argv[ARG_MSG][next_char]);
 		next_char++;
 	}
-	cl_send_bit(server_pid, '\n');
+	// cl_send_bit(server_pid, '\n');
 	exit(EXIT_SUCCESS);
 }
--- src/server/server_main.c
+++ src/server/server_main.c
@@ -6,7 +6,7 @@
 /*   By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/18 13:06:25 by umeneses          #+#    #+#             */
-/*   Updated: 2024/04/26 21:28:08 by umeneses         ###   ########.fr       */
+/*   Updated: 2024/04/27 13:14:06 by umeneses         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,14 +19,20 @@ static void	client_signal_handler(int sig, siginfo_t *info, void *ucontext)
 
 	(void)ucontext;
 	if (sig == SIGUSR1)
+	{
+		ft_putchar_fd('0', STDOUT_FILENO);
 		c += 0 << received_bit;
+	}
 	if (sig == SIGUSR2)
+	{
+		ft_putchar_fd('1', STDOUT_FILENO);
 		c += 1 << received_bit;
+	}
 	received_bit++;
 	if (received_bit == BYTE_SIZE)
 	{
 		ft_putstr_fd(CYAN, STDOUT_FILENO);
-		ft_putstr_fd("########### Client Message ###########\n", STDOUT_FILENO);
+		ft_putstr_fd("\n########### Client Message ###########\n", STDOUT_FILENO);
 		ft_putchar_fd(c, STDOUT_FILENO);
 		received_bit = SIG_DEFAULT;
 		c = SIG_DEFAULT;
@@ -39,21 +45,20 @@ int	main(void)
 {
 	struct sigaction	server_act;
 
-	// sigemptyset(&server_act.sa_mask);
-	ft_memset(&server_act, SIG_DEFAULT, sizeof(server_act));
-	server_act.sa_flags = SA_SIGINFO;
-	server_act.sa_sigaction = client_signal_handler;
-	if ((sigaction(SIGUSR1, &server_act, NULL) == SERVER_FAIL)
-		|| (sigaction(SIGUSR2, &server_act, NULL) == SERVER_FAIL))
-		ft_error_msg("Server signal failed.\n");
 	ft_putstr_fd(GREEN, STDOUT_FILENO);
 	ft_putstr_fd("Server is starting...\n", STDOUT_FILENO);
 	ft_putstr_fd("Your Server PID is >>> ", STDOUT_FILENO);
 	ft_putstr_fd(CYAN, STDOUT_FILENO);
 	ft_putnbr_fd(getpid(), STDOUT_FILENO);
 	ft_putstr_fd(GREEN, STDOUT_FILENO);
-	ft_putstr_fd("\n", STDOUT_FILENO);
-	ft_putstr_fd("Server is running \\o/\n", STDOUT_FILENO);
+	ft_putstr_fd("\nServer is running \\o/\n", STDOUT_FILENO);
+	// sigemptyset(&server_act.sa_mask);
+	ft_memset(&server_act, SIG_DEFAULT, sizeof(server_act));
+	server_act.sa_sigaction = client_signal_handler;
+	server_act.sa_flags = SA_SIGINFO;
+	if ((sigaction(SIGUSR1, &server_act, NULL) == SERVER_FAIL)
+		|| (sigaction(SIGUSR2, &server_act, NULL) == SERVER_FAIL))
+		ft_error_msg("Server signal failed.\n");
 	while (WAIT)
 		pause();
 	exit(EXIT_SUCCESS);
