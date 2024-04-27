6a9d4821249659bed50cbb76b3d13f85b555d02b
umeneses <umeneses@student.42sp.org.br>
2024-04-26 21:31:08 -0300

    trying a new approach on client

--- headers/minitalk.h
+++ headers/minitalk.h
@@ -6,7 +6,7 @@
 /*   By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/15 12:49:06 by umeneses          #+#    #+#             */
-/*   Updated: 2024/04/25 19:53:42 by umeneses         ###   ########.fr       */
+/*   Updated: 2024/04/26 21:09:21 by umeneses         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,7 +37,7 @@ enum e_server
 enum e_client
 {
 	CLIENT_FAIL = -1,
-	MSG_ARG = 2,
+	ARG_MSG = 2,
 	BYTE_SIZE = 8,
 	BIT = 1,
 };
@@ -45,7 +45,7 @@ enum e_client
 enum e_validation
 {
 	ARG_SIZE = 3,
-	PID = 1,
+	ARG_PID = 1,
 };
 
 enum e_signal
@@ -59,9 +59,9 @@ enum e_signal
 
 int		arg_validation(int argc, char *pid_input);
 void	cl_init(int server_pid);
-void	cl_handle_msg(int input_pid, char *msg);
-void	cl_sendingsignal(int input_pid, char *msg);
-void	send_bit(int input_pid, char c);
+// void	cl_send_msg(int server_pid, const char *msg);
+void	cl_sendingsignal(int server_pid, char c_msg);
+void	cl_send_bit(int server_pid, char c_msg);
 /* non-static client functions */
 
 /* non-static server functions */
--- src/client/client_signal.c
+++ src/client/client_signal.c
@@ -6,7 +6,7 @@
 /*   By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/20 17:39:23 by umeneses          #+#    #+#             */
-/*   Updated: 2024/04/25 19:45:44 by umeneses         ###   ########.fr       */
+/*   Updated: 2024/04/26 21:01:10 by umeneses         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,50 @@
 
 static int	ft_connection(int income, int check);
 
-static void	cl_handler_act(int signal)
+static void	server_signal_handler(int signal)
 {
 	if (signal == SIGUSR1)
-	{
-		ft_putstr_fd("Client signal handler SIGUSR>>1 msg..\n", STDOUT_FILENO);
 		ft_connection(SIG_SET, SIG_SET);
-	}
-	if (signal == SIGUSR2)
+}
+
+// void	cl_send_msg(int server_pid, const char *msg)
+// {
+// 	int		index;
+// 	char	*body_msg;
+
+// 	index = 0;
+// 	body_msg = ft_strdup(msg);
+// 	(void)server_pid;
+// 	while (body_msg[index] != '\0')
+// 	{
+// 		// write(STDOUT_FILENO, &body_msg[index], 1);
+// 		cl_send_bit(server_pid, body_msg[index]);
+// 		index++;
+// 	}
+// 	free(body_msg);
+// 	cl_send_bit(server_pid, '\n');
+// }
+
+void	cl_send_bit(int server_pid, char c_msg)
+{
+	int	bit_index;
+	int	bit_signal;
+
+	bit_index = BYTE_SIZE;
+	while (--bit_index >= SIG_DEFAULT)
 	{
-		ft_putstr_fd("Client signal handler SIGUSR>>2 msg..\n", STDOUT_FILENO);
+		ft_connection(SIG_DEFAULT, SIG_SET);
+		bit_signal = (c_msg >> bit_index) & BIT;
+		if (bit_signal == SIG_PENDING_NO)
+		{
+			if (kill(server_pid, SIGUSR1) == CLIENT_FAIL)
+				ft_error_msg("SIGUSR1 - Server offline or invalid PID.\n");
+		}	
+		else if (bit_signal == SIG_PENDING_YES)
+			if (kill(server_pid, SIGUSR2) == CLIENT_FAIL)
+				ft_error_msg("SIGUSR2 - Server offline or invalid PID.\n");
+		while (!ft_connection(SIG_DEFAULT, SIG_GET))
+			;
 	}
 }
 
@@ -36,56 +70,16 @@ static int	ft_connection(int income, int check)
 	return (connection);
 }
 
-void	cl_handle_msg(int input_pid, char *msg)
-{
-	int		index;
-	char	*body_msg;
+// void	cl_sendingsignal(int server_pid, char c_msg)
+// {
+// 	struct sigaction	client_act;
 
-	index = 0;
-	body_msg = ft_strdup(msg);
-	(void)input_pid;
-	while (body_msg[index] != '\0')
-	{
-		// write(STDOUT_FILENO, &body_msg[index], 1);
-		send_bit(input_pid, body_msg[index]);
-		index++;
-	}
-	free(body_msg);
-}
-
-void	send_bit(int input_pid, char c)
-{
-	int	bit_index;
-
-	bit_index = BYTE_SIZE;
-	while (bit_index >= SIG_DEFAULT)
-	{
-		ft_connection(SIG_DEFAULT, SIG_SET);
-		if (((c >> bit_index) & BIT) == SIG_PENDING_NO)
-		{
-			if (kill(input_pid, SIGUSR1))
-				ft_error_msg("SIGUSR1 - Server offline or invalid PID.\n");
-		}	
-		else if (((c >> bit_index) & BIT) == SIG_PENDING_YES)
-			if (kill(input_pid, SIGUSR2))
-				ft_error_msg("SIGUSR2 - Server offline or invalid PID.\n");
-		while (!ft_connection(SIG_DEFAULT, SIG_GET))
-			;
-		bit_index--;
-	}
-}
-
-void	cl_sendingsignal(int input_pid, char *msg)
-{
-	struct sigaction	client_act;
-
-	sigemptyset(&client_act.sa_mask);
-	// ft_memset(&client_act, SIG_DEFAULT, sizeof(client_act));
-	client_act.sa_flags = SA_SIGINFO;
-	// client_act.sa_flags = SIG_DEFAULT;
-	client_act.sa_handler = cl_handler_act;
-	if ((sigaction(SIGUSR1, &client_act, NULL) == CLIENT_FAIL)
-		|| (sigaction(SIGUSR2, &client_act, NULL) == CLIENT_FAIL))
-		ft_error_msg("Client signal failed.\n");
-	cl_handle_msg(input_pid, msg);
-}
+// 	// sigemptyset(&client_act.sa_mask);
+// 	// client_act.sa_flags = SA_SIGINFO;
+// 	ft_memset(&client_act, SIG_DEFAULT, sizeof(client_act));
+// 	client_act.sa_handler = server_signal_handler;
+// 	client_act.sa_flags = SIG_DEFAULT;
+// 	if ((sigaction(SIGUSR1, &client_act, NULL) == CLIENT_FAIL))
+// 		ft_error_msg("Client signal failed.\n");
+// 	cl_send_bit(server_pid, c_msg);
+// }
