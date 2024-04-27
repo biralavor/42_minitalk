df59c6cc1502073a2882cf807ed4309a3a3e0f74
umeneses <umeneses@student.42sp.org.br>
2024-04-27 17:21:00 -0300

    applying 42 norm && norminette

--- src/server/server_main.c
+++ src/server/server_main.c
@@ -6,11 +6,22 @@
 /*   By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/18 13:06:25 by umeneses          #+#    #+#             */
-/*   Updated: 2024/04/27 15:26:56 by umeneses         ###   ########.fr       */
+/*   Updated: 2024/04/27 17:18:37 by umeneses         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
+
+void	sv_init(void)
+{
+	ft_putstr_fd(GREEN, STDOUT_FILENO);
+	ft_putstr_fd("Server is starting...\n", STDOUT_FILENO);
+	ft_putstr_fd("Your Server PID is >>> ", STDOUT_FILENO);
+	ft_putstr_fd(CYAN, STDOUT_FILENO);
+	ft_putnbr_fd(getpid(), STDOUT_FILENO);
+	ft_putstr_fd(GREEN, STDOUT_FILENO);
+	ft_putstr_fd("\nServer is running \\o/\n", STDOUT_FILENO);
+}
 
 static void	client_signal_handler(int sig, siginfo_t *info, void *ucontext)
 {
@@ -19,15 +30,9 @@ static void	client_signal_handler(int sig, siginfo_t *info, void *ucontext)
 
 	(void)ucontext;
 	if (sig == SIGUSR1)
-	{
-		// ft_putchar_fd('0', STDOUT_FILENO);
 		c += 0 << received_bit;
-	}
 	if (sig == SIGUSR2)
-	{
-		// ft_putchar_fd('1', STDOUT_FILENO);
 		c += 1 << received_bit;
-	}
 	received_bit++;
 	if (received_bit == BYTE_SIZE)
 	{
@@ -43,15 +48,8 @@ int	main(void)
 {
 	struct sigaction	server_act;
 
-	ft_putstr_fd(GREEN, STDOUT_FILENO);
-	ft_putstr_fd("Server is starting...\n", STDOUT_FILENO);
-	ft_putstr_fd("Your Server PID is >>> ", STDOUT_FILENO);
-	ft_putstr_fd(CYAN, STDOUT_FILENO);
-	ft_putnbr_fd(getpid(), STDOUT_FILENO);
-	ft_putstr_fd(GREEN, STDOUT_FILENO);
-	ft_putstr_fd("\nServer is running \\o/\n", STDOUT_FILENO);
-	// sigemptyset(&server_act.sa_mask);
-	ft_memset(&server_act, SIG_DEFAULT, sizeof(server_act));
+	sv_init();
+	sigemptyset(&server_act.sa_mask);
 	server_act.sa_sigaction = client_signal_handler;
 	server_act.sa_flags = SA_SIGINFO;
 	if ((sigaction(SIGUSR1, &server_act, NULL) == SERVER_FAIL)
